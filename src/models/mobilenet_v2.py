[Feature fusion] Implemented feature fusion of 2048 FC and averaging - Fixed bug in mobilenet that wasnt pretrained

src/models/mobilenet_v2.py
@@ -1,15 +1,27 @@
 import torch.nn as nn
 import torchvision.models as models
+from torch.hub import load_state_dict_from_url
+from torchvision.models import mobilenet
 
 
 class MobileNetV2(models.MobileNetV2):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes, pretrained=True):
+        self.name = 'mobilenet_v2'
         # Mobilenet by default will use the num_classes to create a classifier with number of neurons in the output
         # of the last fully connected layer are the num_classes. Instead we want to be able to retrieve a feature vector
         # of 2048 on demand instead of the final num_classes vector.
-        super(MobileNetV2, self).__init__(num_classes=2048)
+        super(MobileNetV2, self).__init__()
+
+        if pretrained:
+            state_dict = load_state_dict_from_url(mobilenet.model_urls['mobilenet_v2'], progress=False)
+            self.load_state_dict(state_dict)
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.2, inplace=False),
+            nn.Linear(self.last_channel, 2048),
+        )
+
         self.last_fc = nn.Linear(2048, num_classes)
-        self.name = 'mobilenet_v2'
 
     def forward(self, x, skip_last_fc=False):
         x = super().forward(x)

src/tools/training.py
@@ -173,13 +173,14 @@ def get_confusion_matrix_multiple_models(data_loaders, models, device):
 
 
 @torch.no_grad()
-def get_predictions(data_loader, model, device):
+def get_predictions(data_loader, model, device, skip_last_fc=False):
     """
     Receives a dataloader a model and a device, runs all the batches to get the predictions
     and returns a tensor with all the predictions and corresponding labels for every sample of the dataset
     :param data_loader: Data loader
     :param model: Model
     :param device: Device to be used
+    :param skip_last_fc: whether to return the 2048 vector instead of scores
     :return: predictions, labels
     """
     # Set the model to inference mode
@@ -199,10 +200,13 @@ def get_predictions(data_loader, model, device):
 
     for (data, labels) in data_loader:
         data = data.float().to(device=device)
-        labels = labels.to(device=device)
+        labels = labels.float().to(device=device)
 
-        out, _, _ = _forward(model, data, h, c)
-        scores = functional.softmax(out, 1)
+        out, _, _ = _forward(model, data, h, c, skip_last_fc)
+        if skip_last_fc:
+            scores = out
+        else:
+            scores = functional.softmax(out, 1)
         all_predictions = torch.cat((all_predictions, scores), dim=0)
         all_labels = torch.cat((all_labels, labels), dim=0)
 
@@ -219,21 +223,22 @@ def get_num_correct_predictions(scores, labels):
     return int((labels.argmax(1) == scores.argmax(1)).sum())
 
 
-def _forward(model, data, h, c):
+def _forward(model, data, h, c, skip_last_fc=False):
     """
     Performs a forward pass based on the model given. It automatically handles LSTM, GRU, CNN and other networks.
     :param model: Model to use forward
     :param data: input
     :param h: hidden layer
     :param c: c layer (for lstm)
+    :param skip_last_fc: whether to return the 2048 vector instead of scores
     :return: scores
     """
     if model.name is 'gru':
-        out, h = model(data, h.data)
+        out, h = model(data, h.data, skip_last_fc)
         return out, h, None
     elif model.name is 'lstm':
-        out, h, c = model(data, h.data, c.data)
+        out, h, c = model(data, h.data, c.data, skip_last_fc)
         return out, h, c
     else:
-        out = model(data)
+        out = model(data, skip_last_fc)
         return out, None, None