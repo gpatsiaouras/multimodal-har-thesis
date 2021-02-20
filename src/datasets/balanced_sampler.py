Correct norm_out in every network and fix softmax training

src/datasets/balanced_sampler.py
@@ -18,7 +18,7 @@ class BalancedSampler(BatchSampler):
     def __iter__(self):
         self.count = 0
         while self.count + self.batch_size < len(self.dataset):
-            classes = np.random.choice(self.unique_labels, self.n_classes, replace=False)
+            classes = np.random.choice(self.unique_labels, len(self.unique_labels), replace=False)
             indices = []
             for class_ in classes:
                 class_idx = np.where(self.labels == class_)[0]

src/datasets/utd_mhad.py
@@ -29,6 +29,7 @@ class UtdMhadDataset(Dataset):
         file = open(os.path.join(os.path.dirname(__file__), UTD_MHAD_YAML_CONFIG))
         config = yaml.load(file, Loader=yaml.FullLoader)
         self.actions = config.get('actions')
+        self.class_names = [action['name'] for action in self.actions]
         self.modalities = config.get('modalities')
         self.modality = self.modalities[modality]
         self.joint_names = config.get('joint_names')
@@ -120,7 +121,7 @@ class UtdMhadDataset(Dataset):
         else:
             raise Exception('Unsupported extension: %s' % self.modality['file_ext'])
 
-        actions = np.zeros(len(self.actions))
+        actions = np.zeros(27)
         actions[self.labels[idx] - 1] = 1
 
         if self.transform:
@@ -145,4 +146,4 @@ class UtdMhadDataset(Dataset):
         Used in confusion matrix printing
         :return: list
         """
-        return [action['name'] for action in self.actions]
+        return self.class_names

src/models/bigru.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 
 
 class BiGRU(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+    def __init__(self, input_size, hidden_size, num_layers, out_size, norm_out=False):
         super(BiGRU, self).__init__()
         self.name = 'gru'
         self.num_layers = num_layers
@@ -12,17 +12,19 @@ class BiGRU(nn.Module):
                             bidirectional=self.num_directions == 2)
         self.dropout = nn.Dropout(p=0.8, inplace=True)
         self.fc1 = nn.Linear(hidden_size * self.num_directions, 2048)
-        self.fc2 = nn.Linear(2048, num_classes)
+        self.fc2 = nn.Linear(2048, out_size)
 
-    def forward(self, x, skip_last_fc=False):
+    def forward(self, x):
         out, _ = self.bigru(x)
         # Retrieve only the last state => results to (batch_size, hidden_size)
         out = out[:, -1]
         # Forward to fully connected layers
         out = self.fc1(out)
+        out = self.dropout(out)
+        out = self.fc2(out)
 
-        if not skip_last_fc:
-            out = self.dropout(out)
-            out = self.fc2(out)
+        if self.norm_out:
+            norm = out.norm(p=2, dim=1, keepdim=True)
+            out = out.div(norm)
 
         return out

src/models/mobilenet_v2.py
@@ -5,11 +5,11 @@ from torchvision.models import mobilenet
 
 
 class MobileNetV2(models.MobileNetV2):
-    def __init__(self, num_classes, pretrained=True):
+    def __init__(self, out_size, pretrained=True, norm_out=False):
         self.name = 'mobilenet_v2'
         # Mobilenet by default will use the num_classes to create a classifier with number of neurons in the output
         # of the last fully connected layer are the num_classes. Instead we want to be able to retrieve a feature vector
-        # of 2048 on demand instead of the final num_classes vector.
+        # on demand instead of the final num_classes vector.
         super(MobileNetV2, self).__init__()
 
         if pretrained:
@@ -19,14 +19,16 @@ class MobileNetV2(models.MobileNetV2):
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.2, inplace=False),
             nn.Linear(self.last_channel, 2048),
+            nn.Linear(2048, out_size)
         )
 
-        self.last_fc = nn.Linear(2048, num_classes)
+        self.norm_out = norm_out
 
-    def forward(self, x, skip_last_fc=False):
+    def forward(self, x):
         x = super().forward(x)
 
-        if not skip_last_fc:
-            x = self.last_fc(x)
+        if self.norm_out:
+            norm = x.norm(p=2, dim=1, keepdim=True)
+            x = x.div(norm)
 
         return x

src/train.py
@@ -12,6 +12,8 @@ from datasets import get_transforms_from_config, AVAILABLE_MODALITIES
 from tools import load_yaml, train, get_confusion_matrix, get_accuracy
 from visualizers import print_table, plot_confusion_matrix
 
+torch.manual_seed(0)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--modality', choices=AVAILABLE_MODALITIES, default='inertial')
 parser.add_argument('--gpu', type=int, default=0, help='Only applicable when cuda gpu is available')
@@ -34,17 +36,18 @@ param_config = load_yaml(args.param_file)
 
 # Assign parameters
 modality = args.modality
+modality_config = param_config.get('modalities').get(modality)
 SelectedDataset = getattr(datasets, param_config.get('dataset').get('class_name'))
-transforms, test_transforms = get_transforms_from_config(param_config.get('modalities').get(modality).get('transforms'))
-learning_rate = param_config.get('modalities').get(modality).get('learning_rate') if args.lr is None else args.lr
-batch_size = param_config.get('modalities').get(modality).get('batch_size') if args.bs is None else args.bs
-num_epochs = param_config.get('modalities').get(modality).get('num_epochs') if args.epochs is None else args.epochs
+transforms, test_transforms = get_transforms_from_config(modality_config.get('transforms'))
+learning_rate = modality_config.get('learning_rate') if args.lr is None else args.lr
+batch_size = modality_config.get('batch_size') if args.bs is None else args.bs
+num_epochs = modality_config.get('num_epochs') if args.epochs is None else args.epochs
 shuffle = param_config.get('dataset').get('shuffle')
-model_class_name = param_config.get('modalities').get(modality).get('model').get('class_name')
-criterion = param_config.get('modalities').get(modality).get('criterion').get('class_name')
-criterion_from = param_config.get('modalities').get(modality).get('criterion').get('from_module')
-optimizer = param_config.get('modalities').get(modality).get('optimizer').get('class_name')
-optimizer_from = param_config.get('modalities').get(modality).get('optimizer').get('from_module')
+model_class_name = modality_config.get('model').get('class_name')
+criterion = modality_config.get('criterion').get('class_name')
+criterion_from = modality_config.get('criterion').get('from_module')
+optimizer = modality_config.get('optimizer').get('class_name')
+optimizer_from = modality_config.get('optimizer').get('from_module')
 train_dataset_kwargs = param_config.get('dataset').get('train_kwargs')
 validation_dataset_kwargs = param_config.get('dataset').get('validation_kwargs')
 test_dataset_kwargs = param_config.get('dataset').get('test_kwargs')
@@ -58,7 +61,10 @@ test_dataset = SelectedDataset(modality=modality, transform=test_transforms, **t
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=shuffle)
 
 # Initiate the model
-model = getattr(models, model_class_name)(*param_config.get('modalities').get(modality).get('model').get('args'))
+model = getattr(models, model_class_name)(
+    *modality_config.get('model').get('args'),
+    **modality_config.get('model').get('kwargs')
+)
 model = model.to(device)
 
 # Loss and optimizer