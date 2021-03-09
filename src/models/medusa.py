General fixes. Saved optimized network config for softmax in default.yaml for utd

src/models/medusa.py
@@ -5,22 +5,28 @@ from models import CNN1D, MobileNetV2, MLP
 
 
 class Medusa(nn.Module):
-    def __init__(self, out_size):
+    def __init__(self, mlp_kwargs, n1_kwargs, n2_kwargs, n3_kwargs=None):
         super(Medusa, self).__init__()
         self.name = 'medusa'
-        self.n1 = CNN1D(642, out_size=out_size, norm_out=True)
-        self.n2 = MobileNetV2(out_size, norm_out=True)
-        self.n3 = CNN1D(1107, out_size=out_size, norm_out=True)
-        self.mlp = MLP(out_size * 3, 2048, out_size)
+        self.n1 = CNN1D(**n1_kwargs)
+        self.n2 = MobileNetV2(**n2_kwargs)
+        if n3_kwargs:
+            self.n3 = CNN1D(**n3_kwargs)
+        self.mlp = MLP(**mlp_kwargs)
 
     def forward(self, x):
         (x1, x2, x3) = x
         x1 = self.n1(x1)
         x2 = self.n2(x2)
-        x3 = self.n3(x3)
+        if self.n3:
+            x3 = self.n3(x3)
+            x = torch.cat((x1, x2, x3), dim=1)
+        else:
+            x = torch.cat((x1, x2), dim=1)
+        # L2 Normalize the concatenated vector
+        norm = x.norm(p=2, dim=1, keepdim=True)
+        x = x.div(norm)
 
-        # Concatenate vectors
-        x_all = torch.cat((x1, x2, x3), dim=1)
+        x = self.mlp(x)
 
-        # Pass through mlp
-        return self.mlp(x_all)
+        return x

src/models/mobilenet_v2.py
@@ -5,7 +5,7 @@ from torchvision.models import mobilenet
 
 
 class MobileNetV2(models.MobileNetV2):
-    def __init__(self, out_size, pretrained=True, norm_out=False):
+    def __init__(self, out_size, pretrained=True, norm_out=False, dropout_rate=0.8):
         self.name = 'mobilenet_v2'
         # Mobilenet by default will use the num_classes to create a classifier with number of neurons in the output
         # of the last fully connected layer are the num_classes. Instead we want to be able to retrieve a feature vector
@@ -17,7 +17,7 @@ class MobileNetV2(models.MobileNetV2):
             self.load_state_dict(state_dict)
 
         self.classifier = nn.Sequential(
-            nn.Dropout(p=0.2, inplace=False),
+            nn.Dropout(p=dropout_rate, inplace=False),
             nn.Linear(self.last_channel, 2048),
             nn.Linear(2048, out_size)
         )

src/test_feature_fusion.py
@@ -5,7 +5,7 @@ import torchvision
 from torch.utils.data import DataLoader
 
 from datasets import UtdMhadDataset
-from models import ELM, CNN1D, MobileNetV2, BiLSTM
+from models import ELM, CNN1D, MobileNetV2, BiGRU
 from tools import get_fused_feature_vector, get_predictions
 from transforms import Sampler, FilterDimensions, Flatten, Compose, Normalize, Resize, FilterJoints
 
@@ -37,7 +37,7 @@ train_dataset_inertial = UtdMhadDataset(modality='inertial', subjects=[1, 3, 5, 
     FilterDimensions([0, 1, 2]),
     Flatten(),
 ]))
-test_dataset_inertial = UtdMhadDataset(modality='inertial', subjects=[6, 8], transform=Compose([
+test_dataset_inertial = UtdMhadDataset(modality='inertial', subjects=[2, 4, 6, 8], transform=Compose([
     Normalize(inertial_mean, inertial_std),
     Sampler(107),
     FilterDimensions([0, 1, 2]),
@@ -47,7 +47,7 @@ train_dataset_rgb = UtdMhadDataset(modality='sdfdi', subjects=[1, 3, 5, 7], tran
     torchvision.transforms.Resize(224),
     torchvision.transforms.ToTensor()
 ]))
-test_dataset_rgb = UtdMhadDataset(modality='sdfdi', subjects=[6, 8], transform=Compose([
+test_dataset_rgb = UtdMhadDataset(modality='sdfdi', subjects=[2, 4, 6, 8], transform=Compose([
     torchvision.transforms.Resize(224),
     torchvision.transforms.ToTensor()
 ]))
@@ -57,7 +57,7 @@ train_dataset_skeleton = UtdMhadDataset(modality='skeleton', subjects=[1, 3, 5, 
     FilterJoints(selected_joints),
     Flatten(),
 ]))
-test_dataset_skeleton = UtdMhadDataset(modality='skeleton', subjects=[6, 8], transform=Compose([
+test_dataset_skeleton = UtdMhadDataset(modality='skeleton', subjects=[2, 4, 6, 8], transform=Compose([
     Normalize(skeleton_mean, skeleton_std),
     Resize(num_frames),
     FilterJoints(selected_joints),
@@ -77,7 +77,7 @@ model_inertial = CNN1D(107 * 3, 27).to(device)
 model_inertial.load_state_dict(torch.load(sys.argv[1]))
 model_rgb = MobileNetV2(num_classes, pretrained=False).to(device)
 model_rgb.load_state_dict(torch.load(sys.argv[2]))
-model_skeleton = BiLSTM(input_size_skeleton, hidden_size_skeleton, num_layers, num_classes).to(device)
+model_skeleton = BiGRU(input_size_skeleton, hidden_size_skeleton, num_layers, num_classes).to(device)
 model_skeleton.load_state_dict(torch.load(sys.argv[3]))
 
 # Get predictions from each model for each loader

src/tools/training.py
@@ -74,7 +74,7 @@ def train(model, criterion, optimizer, train_loader, validation_loader, num_epoc
         train_loss = train_running_loss / len(train_loader)
         train_losses.append(train_loss)
         # Validation Loss
-        validation_loss = get_loss(validation_loader, model, device, criterion)
+        validation_loss, _, _ = get_loss(validation_loader, model, device, criterion)
 
         ##############
         # Accuracies #
@@ -88,7 +88,7 @@ def train(model, criterion, optimizer, train_loader, validation_loader, num_epoc
         if len(validation_accuracies) > 0 and validation_acc > max(validation_accuracies):
             if saved_model_path is not None:
                 os.remove(saved_model_path)
-            saved_model_path = save_model(model, '%s.pt' % experiment)
+            saved_model_path = save_model(model, '%s_best_val.pt' % experiment)
         validation_accuracies.append(validation_acc)
         validation_losses.append(validation_loss)
 
@@ -168,18 +168,20 @@ def train_triplet_loss(model, criterion, optimizer, class_names, train_loader, v
             labels_concat = torch.cat((labels_concat, labels), dim=0)
 
         add_histograms(writer, model, epoch)
-        sum_distance_embeddings = torch.sum(torch.cdist(scores_concat, scores_concat))
-        writer.add_scalar('Embeddings/distances', sum_distance_embeddings, global_step=epoch)
+        writer.add_scalar('Embeddings/distances', torch.sum(torch.cdist(scores_concat, scores_concat)),
+                          global_step=epoch)
         # Calculations
         train_loss = train_running_loss / len(train_loader)
         writer.add_scalar('Loss/train', train_loss, global_step=epoch)
-        val_loss = get_loss(val_loader, model, device, criterion)
+        val_loss, val_scores, _ = get_loss(val_loader, model, device, criterion)
+        writer.add_scalar('Embeddings/distances_val', torch.sum(torch.cdist(val_scores, val_scores)),
+                          global_step=epoch)
         writer.add_scalar('Loss/validation', val_loss, global_step=epoch)
         if len(val_losses) > 0:
             # After the first save only for better result in validation
             if val_loss < min(val_losses):
                 os.remove(saved_model_path)
-                saved_model_path = save_model(model, '%s.pt' % experiment)
+                saved_model_path = save_model(model, '%s_best_val.pt' % experiment)
         else:
             # Save the model the first time without checking that the validation was reduced.
             saved_model_path = save_model(model, '%s.pt' % experiment)
@@ -244,7 +246,7 @@ def get_loss(data_loader, model, device, criterion):
     scores, labels = get_predictions(data_loader, model, device, apply_softmax=False)
     loss = criterion(scores, labels.argmax(dim=1))
 
-    return loss.item()
+    return loss.item(), scores, labels
 
 
 def get_accuracy(data_loader, model, device):

src/train.py
@@ -1,7 +1,9 @@
 import argparse
 import importlib
 import random
+import sys
 import time
+import json
 
 import numpy
 import torch
@@ -11,7 +13,7 @@ from torch.utils.tensorboard import SummaryWriter
 import datasets
 import models
 from datasets import get_transforms_from_config, AVAILABLE_MODALITIES
-from tools import load_yaml, train, get_confusion_matrix, get_accuracy
+from tools import load_yaml, train, get_confusion_matrix, get_accuracy, save_model
 from visualizers import print_table, plot_confusion_matrix
 
 # Seed for reproducibility
@@ -24,135 +26,176 @@ random.seed(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--modality', choices=AVAILABLE_MODALITIES, default='inertial')
-parser.add_argument('--gpu', type=int, default=2, help='Only applicable when cuda gpu is available')
-parser.add_argument('--param_file', type=str, default='parameters/utd_mhad/default.yaml')
-parser.add_argument('--epochs', type=int, default=None)
-parser.add_argument('--experiment', type=str, default=None)
-parser.add_argument('--bs', type=int, default=None)
-parser.add_argument('--lr', type=float, default=None)
-parser.add_argument('--dr', type=float, default=None, help='Dropout rate')
-args = parser.parse_args()
 
-# Select device
-cuda_device = 'cuda:%d' % args.gpu
-device = torch.device(cuda_device if torch.cuda.is_available() else 'cpu')
+def main(args):
+    if args.test and args.saved_state is None:
+        print('You have to use --saved_state when using --test, to specify the weights of the model')
+        sys.exit(0)
 
-# Seed number generator
-torch.manual_seed(0)
+    # Select device
+    cuda_device = 'cuda:%d' % args.gpu
+    device = torch.device(cuda_device if torch.cuda.is_available() else 'cpu')
 
-# Load parameters from yaml file.
-param_config = load_yaml(args.param_file)
+    # Load parameters from yaml file.
+    param_config = load_yaml(args.param_file)
 
-# Assign parameters
-modality = args.modality
-modality_config = param_config.get('modalities').get(modality)
-SelectedDataset = getattr(datasets, param_config.get('dataset').get('class_name'))
-transforms, test_transforms = get_transforms_from_config(modality_config.get('transforms'))
-learning_rate = modality_config.get('learning_rate') if args.lr is None else args.lr
-batch_size = modality_config.get('batch_size') if args.bs is None else args.bs
-dropout_rate = modality_config.get('dropout_rate') if args.dr is None else args.dr
-num_epochs = modality_config.get('num_epochs') if args.epochs is None else args.epochs
-shuffle = param_config.get('dataset').get('shuffle')
-model_class_name = modality_config.get('model').get('class_name')
-criterion = modality_config.get('criterion').get('class_name')
-criterion_from = modality_config.get('criterion').get('from_module')
-optimizer = modality_config.get('optimizer').get('class_name')
-optimizer_from = modality_config.get('optimizer').get('from_module')
-train_dataset_kwargs = param_config.get('dataset').get('train_kwargs')
-validation_dataset_kwargs = param_config.get('dataset').get('validation_kwargs')
-test_dataset_kwargs = param_config.get('dataset').get('test_kwargs')
+    # Assign parameters
+    modality = args.modality
+    modality_config = param_config.get('modalities').get(modality)
+    selected_dataset = getattr(datasets, param_config.get('dataset').get('class_name'))
+    transforms, test_transforms = get_transforms_from_config(modality_config.get('transforms'))
+    batch_size = modality_config.get('batch_size') if args.bs is None else args.bs
+    num_epochs = modality_config.get('num_epochs') if args.epochs is None else args.epochs
+    shuffle = param_config.get('dataset').get('shuffle')
+    model_class_name = modality_config.get('model').get('class_name')
+    criterion = modality_config.get('criterion').get('class_name')
+    criterion_from = modality_config.get('criterion').get('from_module')
+    optimizer = modality_config.get('optimizer').get('class_name')
+    optimizer_from = modality_config.get('optimizer').get('from_module')
+    optimizer_kwargs = modality_config.get('optimizer').get('kwargs')
+    if args.lr:
+        optimizer_kwargs['lr'] = args.lr
+    train_dataset_kwargs = param_config.get('dataset').get('train_kwargs')
+    validation_dataset_kwargs = param_config.get('dataset').get('validation_kwargs')
+    test_dataset_kwargs = param_config.get('dataset').get('test_kwargs')
 
-# Load Data
-train_dataset = SelectedDataset(modality=modality, transform=transforms, **train_dataset_kwargs)
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=shuffle)
-validation_dataset = SelectedDataset(modality=modality, transform=test_transforms, **validation_dataset_kwargs)
-validation_loader = DataLoader(dataset=validation_dataset, batch_size=batch_size, shuffle=shuffle)
-test_dataset = SelectedDataset(modality=modality, transform=test_transforms, **test_dataset_kwargs)
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=shuffle)
+    # Load Data
+    train_dataset = selected_dataset(modality=modality, transform=transforms, **train_dataset_kwargs)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=shuffle)
+    validation_dataset = selected_dataset(modality=modality, transform=test_transforms, **validation_dataset_kwargs)
+    validation_loader = DataLoader(dataset=validation_dataset, batch_size=batch_size, shuffle=shuffle)
+    test_dataset = selected_dataset(modality=modality, transform=test_transforms, **test_dataset_kwargs)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=shuffle)
 
-# Initiate the model
-model_kwargs = modality_config.get('model').get('kwargs')
-if args.dr is not None:
-    model_kwargs['dropout_rate'] = args.dr
-model = getattr(models, model_class_name)(
-    *modality_config.get('model').get('args'),
-    **modality_config.get('model').get('kwargs')
-)
-model = model.to(device)
+    # Initiate the model
+    model_kwargs = modality_config.get('model').get('kwargs')
+    if args.dr is not None:
+        model_kwargs['dropout_rate'] = args.dr
+    model = getattr(models, model_class_name)(
+        *modality_config.get('model').get('args'),
+        **modality_config.get('model').get('kwargs')
+    )
+    if args.test:
+        model.load_state_dict(torch.load(args.saved_state))
+    model = model.to(device)
 
-# Loss and optimizer
-criterion = getattr(importlib.import_module(criterion_from), criterion)()
-optimizer = getattr(importlib.import_module(optimizer_from), optimizer)(model.parameters(), learning_rate)
+    # Loss and optimizer
+    criterion = getattr(importlib.import_module(criterion_from), criterion)()
+    optimizer = getattr(importlib.import_module(optimizer_from), optimizer)(model.parameters(), **optimizer_kwargs)
 
-# Initiate Tensorboard writer with the given experiment name or generate an automatic one
-experiment = '%s_%s_%s_%s' % (
-    SelectedDataset.__name__,
-    modality,
-    args.param_file.split('/')[-1],
-    time.strftime("%Y%m%d_%H%M", time.localtime())
-) if args.experiment is None else args.experiment
-writer_name = '../logs/%s' % experiment
-writer = SummaryWriter(writer_name)
+    # Training procedure
+    max_val_acc = -1
+    max_train_acc = -1
+    min_train_loss = -1
+    min_val_loss = -1
 
-# Start training
-train_accs, validation_accs, train_losses, validation_losses = train(
-    model=model,
-    criterion=criterion,
-    optimizer=optimizer,
-    train_loader=train_loader,
-    validation_loader=validation_loader,
-    num_epochs=num_epochs,
-    device=device,
-    experiment=experiment,
-    writer=writer
-)
+    if not args.test:
+        # Initiate Tensorboard writer with the given experiment name or generate an automatic one
+        experiment = '%s_%s_%s_%s' % (
+            selected_dataset.__name__,
+            modality,
+            args.param_file.split('/')[-1],
+            time.strftime("%Y%m%d_%H%M", time.localtime())
+        ) if args.experiment is None else args.experiment
+        writer_name = '../logs/%s' % experiment
+        writer = SummaryWriter(writer_name)
 
-cm_image_train = plot_confusion_matrix(
-    cm=get_confusion_matrix(train_loader, model, device),
-    title='Confusion Matrix - Training',
-    normalize=False,
-    save=False,
-    classes=train_dataset.get_class_names(),
-    show_figure=False
-)
-cm_image_validation = plot_confusion_matrix(
-    cm=get_confusion_matrix(validation_loader, model, device),
-    title='Confusion Matrix - Validation',
-    normalize=False,
-    save=False,
-    classes=validation_dataset.get_class_names(),
-    show_figure=False
-)
-cm_image_test = plot_confusion_matrix(
-    cm=get_confusion_matrix(test_loader, model, device),
-    title='Confusion Matrix - Test',
-    normalize=False,
-    save=False,
-    classes=test_dataset.get_class_names(),
-    show_figure=False
-)
+        # Print parameters
+        print_table({
+            'param_file': args.param_file,
+            'experiment': experiment,
+            'tensorboard_folder': writer_name,
+            'dataset': selected_dataset.__name__,
+            'criterion': type(criterion).__name__,
+            'optimizer': type(optimizer).__name__,
+            'modality': modality,
+            'model': model.name,
+            'learning_rate': optimizer_kwargs['lr'],
+            'batch_size': batch_size,
+            'num_epochs': num_epochs,
+        })
 
-# Add confusion matrices for each dataset, mark it for the last step which is num_epochs - 1
-writer.add_images('ConfusionMatrix/Train', cm_image_train, dataformats='CHW', global_step=num_epochs - 1)
-writer.add_images('ConfusionMatrix/Validation', cm_image_validation, dataformats='CHW', global_step=num_epochs - 1)
-writer.add_images('ConfusionMatrix/Test', cm_image_test, dataformats='CHW', global_step=num_epochs - 1)
+        # Start training
+        train_accs, val_accs, train_losses, val_losses = train(
+            model=model,
+            criterion=criterion,
+            optimizer=optimizer,
+            train_loader=train_loader,
+            validation_loader=validation_loader,
+            num_epochs=num_epochs,
+            device=device,
+            experiment=experiment,
+            writer=writer
+        )
 
-print('Best validation accuracy %f' % max(validation_accs))
-print('Test accuracy %f' % get_accuracy(test_loader, model, device))
+        # Save last state of model
+        save_model(model, '%s_last_state.pt' % experiment)
 
-# Print parameters
-print_table({
-    'param_file': args.param_file,
-    'experiment': experiment,
-    'tensorboard_folder': writer_name,
-    'dataset': SelectedDataset.__name__,
-    'criterion': type(criterion).__name__,
-    'optimizer': type(optimizer).__name__,
-    'modality': modality,
-    'model': model.name,
-    'learning_rate': learning_rate,
-    'batch_size': batch_size,
-    'num_epochs': num_epochs,
-})
+        max_val_acc = max(val_accs) if len(val_accs) > 0 else max_val_acc
+        max_train_acc = max(train_accs) if len(train_accs) > 0 else max_train_acc
+        min_train_loss = max(train_losses) if len(train_losses) > 0 else min_train_loss
+        min_val_loss = max(val_losses) if len(val_losses) > 0 else min_val_loss
+
+        cm_image_train = plot_confusion_matrix(
+            cm=get_confusion_matrix(train_loader, model, device),
+            title='Confusion Matrix - Training',
+            normalize=False,
+            save=False,
+            classes=train_dataset.get_class_names(),
+            show_figure=False
+        )
+        cm_image_validation = plot_confusion_matrix(
+            cm=get_confusion_matrix(validation_loader, model, device),
+            title='Confusion Matrix - Validation',
+            normalize=False,
+            save=False,
+            classes=validation_dataset.get_class_names(),
+            show_figure=False
+        )
+        cm_image_test = plot_confusion_matrix(
+            cm=get_confusion_matrix(test_loader, model, device),
+            title='Confusion Matrix - Test',
+            normalize=False,
+            save=False,
+            classes=test_dataset.get_class_names(),
+            show_figure=False
+        )
+
+        # Add confusion matrices for each dataset, mark it for the last step which is num_epochs - 1
+        writer.add_images('ConfusionMatrix/Train', cm_image_train, dataformats='CHW', global_step=num_epochs - 1)
+        writer.add_images('ConfusionMatrix/Validation', cm_image_validation, dataformats='CHW',
+                          global_step=num_epochs - 1)
+        writer.add_images('ConfusionMatrix/Test', cm_image_test, dataformats='CHW', global_step=num_epochs - 1)
+        print('Best validation accuracy: %f' % max(val_accs))
+
+        writer.add_text('config', json.dumps(param_config, indent=2))
+        writer.add_text('args', json.dumps(args.__dict__, indent=2))
+        writer.flush()
+        writer.close()
+
+    test_accuracy = get_accuracy(test_loader, model, device)
+    print('Test accuracy (not based on val): %f' % test_accuracy)
+
+    return {
+        'test_acc': test_accuracy,
+        'max_train_acc': max_train_acc,
+        'max_val_acc': max_val_acc,
+        'min_train_loss': min_train_loss,
+        'min_val_loss': min_val_loss
+    }
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--modality', choices=AVAILABLE_MODALITIES, default='inertial')
+    parser.add_argument('--test', action='store_true', help='Use this argument to test instead of train')
+    parser.add_argument('--saved_state', type=str, default=None, help='Specify saved model when using --test')
+    parser.add_argument('--gpu', type=int, default=2, help='Only applicable when cuda gpu is available')
+    parser.add_argument('--param_file', type=str, default='parameters/utd_mhad/default.yaml')
+    parser.add_argument('--epochs', type=int, default=None)
+    parser.add_argument('--experiment', type=str, default=None)
+    parser.add_argument('--bs', type=int, default=None)
+    parser.add_argument('--lr', type=float, default=None)
+    parser.add_argument('--dr', type=float, default=None, help='Dropout rate')
+    args = parser.parse_args()
+    main(args)

src/visualize_jittering.py
@@ -1,25 +1,45 @@
+import argparse
+import numpy as np
 from datasets import UtdMhadDataset
 from transforms import Jittering, Compose, FilterDimensions, Sampler
-from visualizers import plot_inertial
+from visualizers import plot_inertial, plot_inertial_gyroscope_multiple
 
 
-# Initiate the dataset with transforms of sampler and filter only gyroscope(x, y, z)
-dataset = UtdMhadDataset(modality='inertial', transform=Compose([
-    Sampler(107),
-    FilterDimensions([0, 1, 2])
-]))
+def main(args):
+    # Initiate the dataset with transforms of sampler and filter only gyroscope(x, y, z)
+    dataset = UtdMhadDataset(modality='inertial', transform=Compose([
+        Sampler(107),
+        FilterDimensions([0, 1, 2])
+    ]))
 
-# Retrieve one sample
-(sample, _) = dataset[150]
-jittering500 = Jittering(500)
-jittering1000 = Jittering(1000)
-jittering1500 = Jittering(1500)
-jittering2000 = Jittering(2000)
-jittering2500 = Jittering(2500)
+    # Retrieve one sample
+    (sample, _) = dataset[args.dataset_idx]
+    if args.jitter_factor:
+        jittering = Jittering(args.jitter_factor)
+        plot_data = jittering(sample)
+        append_to_title = ' - Jittering %d' % args.jitter_factor
+    else:
+        plot_data = sample
+        append_to_title = ' - Original'
 
-plot_inertial(sample, title='Gyroscope - Jittering Original', y_label='deg/sec', save=True)
-plot_inertial(jittering500(sample), title='Gyroscope - Jittering 500', y_label='deg/sec', save=True)
-plot_inertial(jittering1000(sample), title='Gyroscope - Jittering 1000', y_label='deg/sec', save=True)
-plot_inertial(jittering1500(sample), title='Gyroscope - Jittering 1500', y_label='deg/sec', save=True)
-plot_inertial(jittering2000(sample), title='Gyroscope - Jittering 2000', y_label='deg/sec', save=True)
-plot_inertial(jittering2500(sample), title='Gyroscope - Jittering 2500', y_label='deg/sec', save=True)
+    if args.compare:
+        data = np.array([sample[:, 0], plot_data[:, 0]])
+        legends = ['Original', 'Jittered %d' % args.jitter_factor]
+        plot_inertial_gyroscope_multiple(title='Gyroscope', y_label='deg/sec', legends=legends, data=data,
+                                         save=args.save,
+                                         show_figure=args.show)
+    else:
+        plot_inertial(plot_data, title='Gyroscope' + append_to_title, y_label='deg/sec', save=args.save,
+                      show_figure=args.show)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_idx', type=int, default=0, help='idx to plot from train dataset')
+    parser.add_argument('--save', action='store_true')
+    parser.add_argument('--jitter_factor', type=int, default=None)
+    parser.add_argument('--no_show', dest='show', action='store_false')
+    parser.add_argument('--compare', action='store_true')
+    parser.set_defaults(save=False, show=True, compare=False)
+    args = parser.parse_args()
+    main(args)

src/visualize_sampler.py
@@ -1,28 +1,33 @@
 import argparse
-
+import numpy as np
 from datasets import UtdMhadDataset
-from transforms import Compose, FilterDimensions, Sampler
-from visualizers import plot_inertial
+from transforms import Sampler
+from visualizers import plot_inertial, plot_inertial_gyroscope_multiple
 
 
 def main(args):
-    # Initiate the dataset with transforms of sampler and filter only gyroscope(x, y, z)
     dataset = UtdMhadDataset(modality='inertial')
 
     # Retrieve one sample
     (sample, _) = dataset[args.dataset_idx]
+    print('Original step size: %d' % sample.shape[0])
     if args.sampler_size:
         append_to_title = ' - Sampler %d' % args.sampler_size
         sampler = Sampler(args.sampler_size)
         plot_data = sampler(sample)
+        print('New step size: %d' % plot_data.shape[0])
     else:
         append_to_title = ''
         plot_data = sample
 
-    plot_inertial(plot_data[:, :3], title='Gyroscope' + append_to_title, y_label='deg/sec', save=args.save,
-                  show_figure=args.show)
-    plot_inertial(plot_data[:, 3:], title='Accelerometer' + append_to_title, y_label='m/sec^2', save=args.save,
-                  show_figure=args.show)
+    if args.compare:
+        data = np.array([sample[:, 0], plot_data[:, 0]])
+        plot_inertial_gyroscope_multiple(title='Gyroscope', y_label='deg/sec', data=data, save=args.save, show_figure=args.show)
+    else:
+        plot_inertial(plot_data[:, :3], title='Gyroscope' + append_to_title, y_label='deg/sec', save=args.save,
+                      show_figure=args.show)
+        plot_inertial(plot_data[:, 3:], title='Accelerometer' + append_to_title, y_label='m/sec^2', save=args.save,
+                      show_figure=args.show)
 
 
 if __name__ == '__main__':
@@ -31,6 +36,7 @@ if __name__ == '__main__':
     parser.add_argument('--save', action='store_true')
     parser.add_argument('--sampler_size', type=int, default=None)
     parser.add_argument('--no_show', dest='show', action='store_false')
-    parser.set_defaults(save=False, show=True)
+    parser.add_argument('--compare', action='store_true')
+    parser.set_defaults(save=False, show=True, compare=False)
     args = parser.parse_args()
     main(args)